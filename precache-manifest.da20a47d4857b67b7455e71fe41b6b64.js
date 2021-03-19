fb44577fd42adc742d7cf58666132437ff4d32bb
pksatyam777 <pksatyam777@gmail.com>
2021-03-19 16:45:40 +0530

    Updates

---
+++ precache-manifest.da20a47d4857b67b7455e71fe41b6b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86b0870e7cf1a5690748b9a152e9cb7",
+    "url": "/React-Image-Search/index.html"
+  },
+  {
+    "revision": "2b57129c2cc86d64ed60",
+    "url": "/React-Image-Search/static/css/main.744c5823.chunk.css"
+  },
+  {
+    "revision": "1b41a83f67740042f53d",
+    "url": "/React-Image-Search/static/js/2.2b093d78.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Image-Search/static/js/2.2b093d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b57129c2cc86d64ed60",
+    "url": "/React-Image-Search/static/js/main.76eaf220.chunk.js"
+  },
+  {
+    "revision": "130f6376010cea3d61d3",
+    "url": "/React-Image-Search/static/js/runtime-main.161f08cb.js"
+  }
+]);
